72a649d45e2e1ae11b751c84505b910a5879df4a
trongthanh01 <19021367@vnu.edu.vn>
2020-09-23 22:59:33 +0700

    Update output display

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,18 +1,36 @@
-// A BIG MESS
-// NEED TO BE FIXED
-
 public class DictionaryCommandline {
+    DictionaryManagement dictManage = new DictionaryManagement();
+
+    final int spaceNoEng = 9;
+    final int spaceEngViet = 28;
+
+    public void createOutput(int no, String target, String explain) {
+        System.out.print(no);
+        for(int i = 0; i < spaceNoEng - String.valueOf(no).length(); i++) {
+            System.out.print(" ");
+        }
+        System.out.print("| ");
+        System.out.print(target);
+        for(int i = 0; i < spaceEngViet - target.length(); i++) {
+            System.out.print(" ");
+        }
+        System.out.print("| ");
+        System.out.println(explain);
+    }
+
     public void showAllWords() {
-        System.out.println("No       | English                     | Vietnamese");
-        Dictionary dict = new Dictionary();
-        for(int i = 0; i < dict.getLength(); i++) {
-            String newWord = dict.vocab[i].getWordTarget();
-            String newMeaning = dict.vocab[i].getWordExplain();
-            System.out.println((i + 1) + "       | " + newWord + "                     | " + newMeaning);
+        for(int i = -1; i < dictManage.dict.getLength(); i++) {
+            if(i == -1) {
+                System.out.println("No       | English                     | Vietnamese");
+                continue;
+            }
+            String newWord = dictManage.dict.vocab[i].getWordTarget();
+            String newMeaning = dictManage.dict.vocab[i].getWordExplain();
+            createOutput(i + 1, newWord, newMeaning);
         }
     }
+
     public void dictionaryBasic() {
-        DictionaryManagement dictManage = new DictionaryManagement();
         dictManage.insertFromCommandLine();
         showAllWords();
     }
