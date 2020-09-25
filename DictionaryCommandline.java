8a51e3bdbd8cbfac77b1af9f921903ef3e2896f2
trongthanh01 <19021367@vnu.edu.vn>
2020-09-25 23:46:05 +0700

    fix and add functions

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+
 public class DictionaryCommandline {
     DictionaryManagement dictManage = new DictionaryManagement();
 
@@ -5,13 +7,17 @@ public class DictionaryCommandline {
     final int spaceEngViet = 28;
 
     public void createOutput(int no, String target, String explain) {
+        if (no == 0) {
+            System.out.println("No       | English                     | Vietnamese");
+            return;
+        }
         System.out.print(no);
-        for(int i = 0; i < spaceNoEng - String.valueOf(no).length(); i++) {
+        for (int i = 0; i < spaceNoEng - String.valueOf(no).length(); i++) {
             System.out.print(" ");
         }
         System.out.print("| ");
         System.out.print(target);
-        for(int i = 0; i < spaceEngViet - target.length(); i++) {
+        for (int i = 0; i < spaceEngViet - target.length(); i++) {
             System.out.print(" ");
         }
         System.out.print("| ");
@@ -19,19 +25,25 @@ public class DictionaryCommandline {
     }
 
     public void showAllWords() {
-        for(int i = -1; i < dictManage.dict.getLength(); i++) {
-            if(i == -1) {
-                System.out.println("No       | English                     | Vietnamese");
-                continue;
+        for (int i = -1; i < dictManage.dict.getLength(); i++) {
+            if (i != -1) {
+                String newWord = dictManage.dict.vocab[i].getWordTarget();
+                String newMeaning = dictManage.dict.vocab[i].getWordExplain();
+                createOutput(i + 1, newWord, newMeaning);
+            } else {
+                createOutput(i + 1, "", "");
             }
-            String newWord = dictManage.dict.vocab[i].getWordTarget();
-            String newMeaning = dictManage.dict.vocab[i].getWordExplain();
-            createOutput(i + 1, newWord, newMeaning);
         }
     }
 
     public void dictionaryBasic() {
-        dictManage.insertFromCommandLine();
+        // dictManage.insertFromCommandLine();
+        try {
+            dictManage.insertFromFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot read file!");
+            e.printStackTrace();
+        }
         showAllWords();
     }
 }
--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class DictionaryManagement {
     public Dictionary dict = new Dictionary();
 
@@ -7,9 +10,49 @@ public class DictionaryManagement {
         int numberOfWords = sc.nextInt();
         sc.nextLine();
         dict.setLength(numberOfWords);
-        for(int i = 0; i < numberOfWords; i++) {
+        for (int i = 0; i < numberOfWords; i++) {
             dict.vocab[i].setWordTarget(sc.nextLine());
             dict.vocab[i].setWordExplain(sc.nextLine());
+        }
+        sc.close();
+    }
+
+    public int findLengthInFile() throws FileNotFoundException {
+        File file = new File("dictionaries.txt");
+        Scanner sc = new Scanner(file);
+        int length = 0;
+        while(sc.hasNextLine()) {
+            sc.nextLine();
+            length++;
+        }
+        sc.close();
+        return length;
+    }
+
+    public void insertFromFile() throws FileNotFoundException {
+        File file = new File("dictionaries.txt");
+        Scanner sc = new Scanner(file);
+
+        // Create an object for vocab
+        int length = findLengthInFile();
+        dict.setLength(length);
+        int i = 0;
+
+        // Read from file
+        while(sc.hasNextLine()) {
+            dict.vocab[i] = new Word();
+            String newWord = sc.next();
+            dict.vocab[i].setWordTarget(newWord);
+            String newMeaning1 = sc.next();
+            String newMeaning2;
+            if(sc.hasNextLine()) {
+                newMeaning2 = sc.nextLine();
+            }
+            else {
+                newMeaning2 = "";
+            }
+            dict.vocab[i].setWordExplain(newMeaning1 + newMeaning2);
+            i++;
         }
         sc.close();
     }
