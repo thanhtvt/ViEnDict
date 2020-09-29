6cc26c2f115a1e03a22d5113984dab0f9f28e402
trongthanh01 <19021367@vnu.edu.vn>
2020-09-29 10:35:50 +0700

    TST algo

---
+++ TernarySearchTree.java
@@ -0,0 +1,98 @@
+import java.util.Iterator;
+import java.util.Vector;
+
+public class TernarySearchTree {
+    private Node root;
+    private Vector<String> traversedArr = new Vector<>();
+
+    private Node insertUtil(Node r, char[] word, int pos) {
+        if (r == null) {
+            r = new Node(word[pos]);
+        }
+ 
+        if (word[pos] < r.getData()) {
+            r.left = insertUtil(r.left, word, pos);
+        } 
+        else if (word[pos] > r.getData()) {
+            r.right = insertUtil(r.right, word, pos);
+        } 
+        else {
+            if (pos + 1 < word.length) {
+                r.middle = insertUtil(r.middle, word, pos + 1);
+            } 
+            else r.setEnding(true);
+        }
+        return r;
+    }
+    public void insert(String word) {
+        root = insertUtil(root, word.toCharArray(), 0);
+    }
+
+    private void traverse(Node r, String pattern, char[] word, int depth) {
+        if (r != null) 
+        { 
+            // First traverse the left subtree 
+            traverse(r.left, pattern, word, depth); 
+    
+            // Store the character of this node 
+            word[depth] = r.getData(); 
+            if (r.getEnding()) 
+            { 
+                word[depth+1] = '\0'; 
+                traversedArr.add(pattern + String.valueOf(word)); 
+            } 
+    
+            // Traverse the subtree using middle pointer 
+            traverse(r.middle, pattern, word, depth + 1); 
+    
+            // Finally Traverse the right subtree 
+            traverse(r.right, pattern, word, depth); 
+        } 
+    }
+
+    private void autoCompleteArr(Node r, String pattern) {
+        char[] word = new char[100];
+        if(r.getEnding()) {
+            traversedArr.add(pattern);
+        }
+        traverse(r, pattern, word, 0);
+    }
+
+    private void nodeIdentify(Node r, String pattern, int pos) {
+        char[] input = pattern.toCharArray();
+        while(r != null && pos < pattern.length()) {
+            int compareChars = Character.compare(r.getData(), input[pos]);
+            if(compareChars > 0) {
+                r = r.left;
+            }
+            else if(compareChars < 0) {
+                r = r.right;
+            }
+            else if(compareChars == 0) {
+                r = r.middle;
+                pos++;
+            }
+        }
+        if(r == null) {
+            System.out.println("NOT FOUND!");
+            return;
+        }
+        autoCompleteArr(r, pattern);
+    }
+
+    public void autoComplete(String pattern) {
+        if(pattern.isEmpty()) {
+            return; 
+        }
+        // Identify starting node in TST
+        nodeIdentify(root, pattern, 0);
+        printArray();
+    }
+    
+    private void printArray() {
+        Iterator<String> value = traversedArr.iterator();
+        while (value.hasNext()) { 
+            System.out.println(value.next()); 
+        }
+    }
+}
