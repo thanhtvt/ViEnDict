b66f281ce29c7420cd96b5fad621b9613f94ef74
trongthanh01 <19021367@vnu.edu.vn>
2020-09-29 10:35:56 +0700

    main

--- Run.java
+++ Run.java
@@ -1,7 +1,9 @@
 public class Run {
     public static void main(String[] args) {
         DictionaryCommandline dictCom = new DictionaryCommandline();
-        //dictCom.dictionaryBasic();
-        dictCom.dictionaryAdvanced();
+        dictCom.dictionaryBasic();
+        //dictCom.dictionaryAdvanced();
+        dictCom.dictManage.addWord();
+        dictCom.dictionarySearcher();
     }
 }
