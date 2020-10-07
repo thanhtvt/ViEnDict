3d02232573bba6b0d3aa56c3647dc598fb8759aa
trongthanh01 <19021367@vnu.edu.vn>
2020-10-07 21:43:25 +0700

    update

--- Dictionary.java
+++ Dictionary.java
@@ -1,6 +1,14 @@
 public class Dictionary {
     private int length;
+    private final int SIZE_OF_DICT = 60000;
     public Word[] vocab;
+
+    public Dictionary() {
+        vocab = new Word[SIZE_OF_DICT];
+        for(int i = 0; i < SIZE_OF_DICT; i++) {
+            vocab[i] = new Word();
+        }
+    }
 
     public void setLength(int len) {
         length = len;
