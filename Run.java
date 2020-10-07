e33eec34102fd16b074854f07004e837a7cf0042
trongthanh01 <19021367@vnu.edu.vn>
2020-10-07 21:42:20 +0700

    main

--- Run.java
+++ Run.java
@@ -1,12 +1,17 @@
 public class Run {
     public static void main(String[] args) {
-        DictionaryCommandline dictCom = new DictionaryCommandline();
-        dictCom.dictionaryBasic();
+        //DictionaryCommandline dictCom = new DictionaryCommandline();
+        DictionaryApplication dictApp = new DictionaryApplication();
+
+        //dictCom.dictionaryBasic();
+        //dictCom.dictManage.insertFromFile();
         //dictCom.dictionaryAdvanced();
-        dictCom.dictManage.addWord();
+        //dictCom.dictManage.addWord();
         //dictCom.dictionarySearcher();
-        dictCom.dictManage.deleteWord("hello");
-        dictCom.showAllWords();
-        dictCom.dictManage.dictionaryExportToFile();
+        //dictCom.dictManage.deleteWord("hello");
+        //dictCom.showAllWords();
+        //dictCom.dictManage.dictionaryExportToFile();
+        
+        dictApp.runApplication();
     }
 }
